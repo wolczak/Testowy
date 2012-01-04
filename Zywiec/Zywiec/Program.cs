468eb2af1b0e809f280e9db159dc1d401bad4b67
wolczak <wolczak@future-processing.com>
2012-01-04 23:05:41 +0100

    Nowa testowa solucja o nazwie zywiec

---
+++ Zywiec/Zywiec/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Zywiec
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("dupa");
+            Console.ReadLine();
+        }
+    }
+}
