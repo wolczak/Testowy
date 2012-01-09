33448a6ddc3eeafc7ad2bc53f5b8075b87f4e645
wolczak <wolczak@future-processing.com>
2012-01-09 16:26:44 +0100

    Clean up test solution

---
+++ HibernateTest/WebTest.Core/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebTest.Core
+{
+    public class Product
+    {
+        public virtual decimal ID { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual decimal Quantity { get; set; }
+    }
+}
--- HibernateTest/WebTest/Default.aspx.cs
+++ HibernateTest/WebTest/Default.aspx.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using HibernateTest;
 using NHibernate.Cfg;
+using WebTest.Core;
 
 namespace WebTest
 {
